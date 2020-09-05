480f3851045e6116f2eaf2b5f97c3ababe6e32dc
TEAM-QWERT <69754972+TEAM-QWERT@users.noreply.github.com>
2020-09-05 11:30:29 +0900

    Add files via upload

--- burger_war/scripts/qwerty.py
+++ burger_war/scripts/qwerty.py
@@ -81,6 +81,7 @@ class Commander(smach.State):
             
         #各状況に合わせて状態遷移
         if (rospy.Time.now() - self.last_flag_reset_time) < self.flag_reset_wait:
+            overlaytext.publish('STATE: Commander')
             return "commander"
          #敵が近くにいる場合、fightEnemy状態に遷移
         if self.close_enemy == True:
